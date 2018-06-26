2348cdb2981e2bbe1efb2642c422c7a19657f4e8
Andrew Nead <andrewnead@andrews-mbp-2.coldspring.marian.edu>
2018-06-26 14:39:23 -0400

    Added second list for birthdays.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const btn1 = document.querySelector('#btn1')
 const btn2 = document.querySelector('#btn2')
-const ent = document.querySelector('input')
+const btn3 = document.querySelector('#btn3')
+const mov = document.querySelector('#Movies')
+const bdy = document.querySelector('#birthdates')
 const flicks = document.querySelector('#flicks')
+const bdys= document.querySelector('#Birthdays')
 
 const headingChange = function() {
     const frstHead = document.querySelector('#Title')
@@ -10,23 +13,43 @@ const headingChange = function() {
 
 const addMovie = function() {
     let item = document.createElement('li')
-        item.textContent= ent.value
+        item.textContent= mov.value
         flicks.appendChild(item)
 
-        ent.value= ""
-        ent.focus()
+        mov.value= ""
+        mov.focus()
 }
 const addMovieByEnter = function(par ) {
     if (par.key == "Enter") {
         
         let item = document.createElement('li')
-        item.textContent= ent.value
+        item.textContent= mov.value
         flicks.appendChild(item)
 
-        ent.value= ""
+        mov.value= ""
+    }
+}
+const addBirthday = function() {
+    let item = document.createElement('li')
+        item.textContent= bdy.value
+        bdys.appendChild(item)
+
+        bdy.value= ""
+        bdy.focus()
+}
+const addBirthdayEnter = function(par ) {
+    if (par.key == "Enter") {
+        
+        let item = document.createElement('li')
+        item.textContent= bdy.value
+        bdys.appendChild(item)
+
+        bdy.value= ""
     }
 }
 
 btn1.addEventListener('click', headingChange)
 btn2.addEventListener('click', addMovie)
-ent.addEventListener('keydown', addMovieByEnter) 
+btn3.addEventListener('click', addBirthday)
+mov.addEventListener('keydown', addMovieByEnter)
+bdy.addEventListener('keydown', addBirthdayEnter)
