a53eff7f23fe302102841355242a9e974a09e0f1
Andrew Nead <andrewnead@andrews-mbp-2.coldspring.marian.edu>
2018-06-26 14:00:28 -0400

    Uses createElement and appendChild

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const btn1 = document.querySelector('#btn1')
 const btn2 = document.querySelector('#btn2')
 const btn3 = document.querySelector('#btn3')
 const ent = document.querySelector('input')
-
+const flicks = document.querySelector('#flicks')
 
 const headingChange = function() {
     const frstHead = document.querySelector('#Title')
@@ -13,13 +13,19 @@ const emoteChange = function() {
     scndHead.textContent= 'I am Happy'
 }
 const txtBoxHead = function() {
-    const frstHead = document.querySelector('#Title')
-    frstHead.textContent = ent.value
+    let item = document.createElement('li')
+        item.textContent= ent.value
+        flicks.appendChild(item)
+
+        ent.value= ""
+        ent.focus()
 }
 const txtBoxEnt = function(par ) {
     if (par.key == "Enter") {
-        const flicksDiv = document.querySelector('#flicks')
-        flicksDiv.innerHTML += `<li>` + ent.value + `</li>`
+        
+        let item = document.createElement('li')
+        item.textContent= ent.value
+        flicks.appendChild(item)
 
         ent.value= ""
     }
@@ -28,4 +34,4 @@ const txtBoxEnt = function(par ) {
 btn1.addEventListener('click', headingChange)
 btn2.addEventListener('click', emoteChange)
 btn3.addEventListener('click', txtBoxHead)
-ent.addEventListener('keydown', txtBoxEnt)
+ent.addEventListener('keydown', txtBoxEnt) 
