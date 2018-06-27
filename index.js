cc0f7f228bb13ed81c8c8fe7140cc402bdd50171
Andrew Nead <andrewnead@andrews-mbp-2.coldspring.marian.edu>
2018-06-27 08:33:07 -0400

    Combined the second list with the first.

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const btn1 = document.querySelector('#btn1')
 const btn2 = document.querySelector('#btn2')
-const btn3 = document.querySelector('#btn3')
 const mov = document.querySelector('#Movies')
-const bdy = document.querySelector('#birthdates')
+const yr = document.querySelector('#Yr')
 const flicks = document.querySelector('#flicks')
-const bdys= document.querySelector('#Birthdays')
 
 const headingChange = function() {
     const frstHead = document.querySelector('#Title')
@@ -13,43 +11,25 @@ const headingChange = function() {
 
 const addMovie = function() {
     let item = document.createElement('li')
-        item.textContent= mov.value
+        item.textContent= mov.value + "(" + yr.value + ")"
         flicks.appendChild(item)
 
         mov.value= ""
+        yr.value = ""
         mov.focus()
 }
 const addMovieByEnter = function(par ) {
     if (par.key == "Enter") {
         
         let item = document.createElement('li')
-        item.textContent= mov.value
+        item.innerHTML= `<span><font color="Darkred">mov.value </font></span><span><font color="#A07FC0">"(" + yr.value + ")"</font></span>`
         flicks.appendChild(item)
 
         mov.value= ""
     }
 }
-const addBirthday = function() {
-    let item = document.createElement('li')
-        item.textContent= bdy.value
-        bdys.appendChild(item)
-
-        bdy.value= ""
-        bdy.focus()
-}
-const addBirthdayEnter = function(par ) {
-    if (par.key == "Enter") {
-        
-        let item = document.createElement('li')
-        item.textContent= bdy.value
-        bdys.appendChild(item)
-
-        bdy.value= ""
-    }
-}
 
 btn1.addEventListener('click', headingChange)
 btn2.addEventListener('click', addMovie)
-btn3.addEventListener('click', addBirthday)
 mov.addEventListener('keydown', addMovieByEnter)
-bdy.addEventListener('keydown', addBirthdayEnter)
+yr.addEventListener('keydown', addMovieByEnter)
