8ab5ba57fa2ea074bac3b27645b5a67d73ab807f
Andrew Nead <andrewnead@andrews-mbp-2.coldspring.marian.edu>
2018-06-27 14:36:40 -0400

    Reworked to use a form tag.

--- index.js
+++ index.js
@@ -2,30 +2,18 @@ const btn2 = document.querySelector('#btn2')
 const mov = document.querySelector('#Movies')
 const yr = document.querySelector('#Yr')
 const flicks = document.querySelector('#flicks')
+const form = document.querySelector('form')
 
+const SubmitHandler= function(ev) {
+ev.preventDefault()
+let item = document.createElement('li')
+item.innerHTML= `<span><font color="Darkred">${mov.value}</font></span> <span><font color="#A07FC0">(${yr.value})</font></span>`
+flicks.appendChild(item)
 
-const addMovie = function() {
-    let item = document.createElement('li')
-    item.innerHTML= `<span><font color="Darkred">${mov.value}</font></span> <span><font color="#A07FC0">(${yr.value})</font></span>`
-        flicks.appendChild(item)
-
-        mov.value= ""
-        yr.value = ""
-        mov.focus()
-}
-const addMovieByEnter = function(par ) {
-    if (par.key == "Enter") {
-        
-        let item = document.createElement('li')
-        item.innerHTML= `<span><font color="Darkred">${mov.value}</font></span> <span><font color="#A07FC0">(${yr.value})</font></span>`
-        flicks.appendChild(item)
-
-        mov.value= ""
-        yr.value=""
-        mov.focus()
-    }
+mov.value= ""
+yr.value = ""
+mov.focus()
 }
 
-btn2.addEventListener('click', addMovie)
-mov.addEventListener('keydown', addMovieByEnter)
-yr.addEventListener('keydown', addMovieByEnter)
+
+form.addEventListener('submit', SubmitHandler)
