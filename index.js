b63bf8e82fc917e8767f8561aa20f7f9d2a7e879
Andrew Nead <andrewnead@andrews-mbp-2.coldspring.marian.edu>
2018-06-28 16:43:19 -0400

    Extracted renderProp and renderItem

--- index.js
+++ index.js
@@ -4,11 +4,25 @@ const yr = document.querySelector('#Yr')
 const flicks = document.querySelector('#flicks')
 const form = document.querySelector('form')
 
+const renderProp = function(name, val){
+const span = document.createElement('span')
+span.classList.add(name)
+span.textContent=val
+return span
+}
+
+const renderItem = function(mvSpan,yrSpan){
+let item = document.createElement('li')
+item.appendChild(mvSpan)
+item.appendChild(yrSpan)
+flicks.appendChild(item)
+}
+
 const SubmitHandler= function(ev) {
 ev.preventDefault()
-let item = document.createElement('li')
-item.innerHTML= `<span><font color="Darkred">${mov.value}</font></span> <span><font color="#A07FC0">(${yr.value})</font></span>`
-flicks.appendChild(item)
+const movSpan = renderProp("Flick", mov.value)
+const yrSpan = renderProp("Created", yr.value)
+renderItem(movSpan, yrSpan)
 
 mov.value= ""
 yr.value = ""
