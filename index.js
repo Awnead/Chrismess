8bb71b7312756d9407d4596820606e9483708938
Andrew Nead <andrewnead@andrews-mbp-2.coldspring.marian.edu>
2018-06-28 17:57:19 -0400

    Use an ES6 class and arrow functions

--- index.js
+++ index.js
@@ -1,33 +1,48 @@
-const btn2 = document.querySelector('#btn2')
-const mov = document.querySelector('#Movies')
-const yr = document.querySelector('#Yr')
-const flicks = document.querySelector('#flicks')
-const form = document.querySelector('form')
+class App {
+    constructor() {
+        const form = document.querySelector('form')
+        form.addEventListener('submit', (ev) => {
+            ev.preventDefault()
+            this.SubmitHandler(ev)})
+    }
 
-const renderProp = function(name, val){
-const span = document.createElement('span')
-span.classList.add(name)
-span.textContent=val
-return span
+        renderProp(name, val){
+        const span = document.createElement('span')
+        span.classList.add(name)
+        span.textContent=val
+        return span
+        }
+        
+        renderItem(mvSpan,yrSpan){
+        let item = document.createElement('li')
+        item.appendChild(mvSpan)
+        item.appendChild(yrSpan)
+        const flicks = document.querySelector('#flicks')
+        flicks.appendChild(item)
+        }
+        
+
+
+        SubmitHandler(ev) {
+        const mov = document.querySelector('#Movies')
+        const yr = document.querySelector('#Yr')
+        
+        const movSpan = this.renderProp("Flick", mov.value)
+        const yrSpan = this.renderProp("Created", yr.value)
+        this.renderItem(movSpan, yrSpan)
+        
+        mov.value= ""
+        yr.value = ""
+        mov.focus()
+        }
+        
+        
+        
 }
 
-const renderItem = function(mvSpan,yrSpan){
-let item = document.createElement('li')
-item.appendChild(mvSpan)
-item.appendChild(yrSpan)
-flicks.appendChild(item)
-}
-
-const SubmitHandler= function(ev) {
-ev.preventDefault()
-const movSpan = renderProp("Flick", mov.value)
-const yrSpan = renderProp("Created", yr.value)
-renderItem(movSpan, yrSpan)
-
-mov.value= ""
-yr.value = ""
-mov.focus()
-}
+const app = new App()
 
 
-form.addEventListener('submit', SubmitHandler)
+
+
+
